29a5deded210f6ee8647954ad73812a1e5557c0f
Joao Almeida <wagner.dev1337@gmail.com>
2020-01-30 18:21:36 -0300

    configurando o serializer de course

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,3 +1,7 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id
+  
+  attributes :id, :period, :name, :content, :weekTime, :totalTime, :kind, :teacher
+
+  has_many :books
+
 end

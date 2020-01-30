d3d77456006b8ae184cfd65afe27e7fd340e26d6
Joao Almeida <wagner.dev1337@gmail.com>
2020-01-30 18:12:47 -0300

    migração do database e população do db com seeds

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ApplicationRecord
+
+    has_many :books
+
 end
---
+++ db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_01_30_201059) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "isbn"
+    t.integer "kind"
+    t.string "teacher"
+    t.integer "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_books_on_course_id"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.integer "period"
+    t.string "name"
+    t.string "content"
+    t.integer "weekTime"
+    t.integer "totalTime"
+    t.integer "kind"
+    t.string "teacher"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "books", "courses"
+end
